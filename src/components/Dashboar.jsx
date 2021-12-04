a6b8770e92073a43acebb1398fd41ae0255e4047
Cristianr <calrod350@gmail.com>
2021-12-03 20:08:55 -0500

    detalles

--- src/components/Dashboar.jsx
+++ src/components/Dashboar.jsx
@@ -34,7 +34,7 @@ const Dashboar = () =>{
     <hr />
     <div className="dropdown">
       <p className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2" />
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" width="32" height="32" class="rounded-circle me-2" />
         <strong>mdo</strong>
       </p>
       <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">

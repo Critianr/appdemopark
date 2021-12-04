34e7ce21767e30ecedec4ddfdfd5a78c3a51e11f
Cristianr <calrod350@gmail.com>
2021-12-03 20:41:18 -0500

    detalles 2

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Notfound from '../assets/img/404.jpg'
+import '../assets/css/not.css'
 export default function NotFound() {
         return (
             <div>
-                <h1>404</h1>
-                <h2>Not Found</h2>                
+            <h2>Pagina No encontrada</h2>
+            <div className="container-fluid not">
+                
+                <img src={Notfound} width="800" height="500" alt="notfound" />
+            </div>
             </div>
         )
 }
